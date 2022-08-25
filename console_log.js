88b2add1b5372cf4363dde6c48deb5c70d8deb18
LindiweKhumalo <86086701+Shantralee-58@users.noreply.github.com>
2022-08-25 21:37:41 +0200

    Update console_log.js

--- console_log.js
+++ console_log.js
@@ -1,1 +1,4 @@
-
+var name = " Lindiwe Khumalo";
+var institution = " MTN App Academy";
+var Github_username = " Shantralee-58"
+console.log("My name is" + name, ", a student from" + institution, "and my Github username is" +Github_username);
